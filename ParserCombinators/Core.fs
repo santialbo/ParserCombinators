2abfc1826fe6fdb39e58c34d2f42e1ff2de05e03
santi <santialbo@gmail.com>
2013-03-20 18:37:58 -0400

    Added function description comments

--- ParserCombinators/Core.fs
+++ ParserCombinators/Core.fs
@@ -7,33 +7,41 @@ type ParserResult<'a> =
     
 type Parser<'a> = list<char> -> ParserResult<'a>
 
+/// Bind operator. Applies f to the result of parser p.
 let (>>=) (p: Parser<'a>) (f: 'a -> Parser<'b>) : Parser<'b> =
     fun stream ->
         match p stream with
         | Success(x, rest) -> (f x) rest
         | Failure -> Failure
 
+/// Always returns a Success with x as result
 let preturn x : Parser<'a>=
     fun stream -> Success(x, stream)
-    
+
+/// Always fails.
 let pzero : Parser<'a> =
     fun stream -> Failure
-    
+
+/// Computation expression builder.
 type ParserBuilder() =
     member x.Bind(p, f) = p >>= f
     member x.Return(y) = preturn y
 
 let parse = new ParserBuilder()
 
+/// If p1 succeeds returns the result x.
 let (>>%) p1 x : Parser<'b> =
     p1 >>= (fun _ -> preturn x)
 
+/// Applies p1 and p2 returning the result of p2.
 let (>>.) p1 p2 : Parser<'b> =
     p1 >>= (fun _ -> p2)
 
+/// Applies p1 and p2 returning the result of p1.
 let (.>>) p1 p2 : Parser<'a> =
     p1 >>= (fun x -> p2 >>% x)
     
+/// Applies p1 and p2 returning as a result a tuple with both results.
 let (.>>.) p1 p2: Parser<'a*'b> =
     p1 >>= (fun x -> p2 >>= (fun y -> preturn (x, y)))
 
@@ -46,60 +54,61 @@ let (<|>) (p1: Parser<'a>) (p2: Parser<'a>) : Parser<'a> =
 
 let (|>>) p f : Parser<'b> =
     p >>= (fun x -> preturn (f x))
-    
+
+/// Runs p as many times as posible, returning a list with the results.
 let rec Many p : Parser<list<'a>> =
     parse { let! x = p
             let! xs = (Many p)
             return x :: xs
            } <|> preturn []
 
+/// Runs p as many times as possible with at least one Succcess.
 let Many1 p : Parser<list<'a>> =
     parse { let! x = p
             let! xs = (Many p)
             return x :: xs
           }
 
+/// Returns the first successful result of the given parser sequence.
 let Choice ps : Parser<'a> =
     Seq.reduce (fun (p1: Parser<'a>) (p2: Parser<'a>) -> p1 <|> p2) ps
     
+/// Runs the given parser against the given string.
 let Run (p: Parser<'a>) (s: string) =
     p (Seq.toList s)
 
+/// Parses the given character.
 let CharParser c : Parser<char> =
     let p = function
         | x::xs when x = c -> Success(x, xs)
         | _ -> Failure
     in p
-
+    
+/// Parses characters which satisfy the given function.
 let CharParserF f: Parser<char> =
     let p = function
         | x::xs when f x -> Success(x, xs)
         | _ -> Failure
     in p
-    
-let rec CharListParser (cs: list<char>) =
-    match cs with
-    | [] -> preturn []
-    | c::cs' -> parse { let! c' = CharParser c
-                        let! cs'' = CharListParser cs'
-                        return c'::cs''
-                      }
-        
-let rec CharListParser2 (cs: list<char>) =
-    match cs with
-    | [] -> preturn []
-    | c::cs' ->
-        (CharParser c) >>= (fun c' ->
-            (CharListParser2 cs') >>= (fun cs'' -> preturn (c'::cs'')))
 
+/// Parses the given string.
 let StringParser (s: string) =
+    let rec CharListParser (cs: list<char>) =
+        match cs with
+        | [] -> preturn []
+        | c::cs' -> parse { let! c' = CharParser c
+                            let! cs'' = CharListParser cs'
+                            return c'::cs''
+                          }
     CharListParser (Seq.toList s) |>> (fun cs -> new System.String(cs |> List.toArray))
-    
+
+/// Parses white space characters. Useful for skipping them.
 let WhiteSpaceParser =
     [' '; '\t'; '\n'; '\r']
     |> List.map CharParser
     |> Choice
 
+/// Parses integer number which match /[+-]?\d+/
 let IntegerParser : Parser<int> =
     parse {
         let! s = (CharParser '+' <|> CharParser '-') <|> preturn '+'
@@ -107,23 +116,25 @@ let IntegerParser : Parser<int> =
         return int (new System.String(s::d |> List.toArray))
     }
 
+/// Parses float numbers which match /[+-]?\d+(\.\d*)?([eE][+-]?\d+)?/
 let FloatParser: Parser<float> =
     parse {
-        let! s = (CharParser '+' <|> CharParser '-') <|> preturn '+'
-        let! l = Many (CharParserF (fun c -> c >= '0' && c <= '9'))
+        let! s = (CharParser '+' <|> CharParser '-') <|> preturn '+'      // [+-]?
+        let! l = Many (CharParserF (fun c -> c >= '0' && c <= '9'))       // \d+
         let! d = (parse {
-            let! p = CharParser '.'
-            let! d = Many (CharParserF (fun c -> c >= '0' && c <= '9'))
-            return p::d
+            let! p = CharParser '.'                                       // (\.
+            let! d = Many (CharParserF (fun c -> c >= '0' && c <= '9'))   //  \d*
+            return p::d                                                   //     )?
         } <|> preturn [])
         let! e = (parse {
-            let! e = CharParser 'e' <|> CharParser 'E'
-            let! s = (CharParser '+' <|> CharParser '-') <|> preturn '+'
-            let! x = Many1 (CharParserF (fun c -> c >= '0' && c <= '9'))
-            return e::s::x
+            let! e = CharParser 'e' <|> CharParser 'E'                    // ([eE]
+            let! s = (CharParser '+' <|> CharParser '-') <|> preturn '+'  //   [+-]?
+            let! x = Many1 (CharParserF (fun c -> c >= '0' && c <= '9'))  //   \d+
+            return e::s::x                                                //         )?
         } <|> preturn [])
         return float (new System.String(s::(l @ d @ e) |> List.toArray))
     }
-    
-let Between p1 p2 p =
-    p1 >>. p .>> p2
+
+/// Applies the parsers popen, p and pclose in sequence. It returns the result of p.
+let Between popen pclose p =
+    popen >>. p .>> pclose
