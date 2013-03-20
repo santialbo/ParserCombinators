2dd5e3877d8891983ef52c8170b8555b7b265d17
santi <santialbo@gmail.com>
2013-03-20 16:35:16 -0400

    Added unit testing. Tests made for IntegerParser and FloatParser

--- ParserCombinators/Program.fs
+++ ParserCombinators/Program.fs
@@ -5,19 +5,7 @@ open ParserCombinators.Core
 open ParserCombinators.Json
 
 
-         
-
 [<EntryPoint>]
 let main args =
-    let a = FloatParser
-    printfn "%A" (Run a @"-.2")
-    printfn "%A" (Run a @"-1.2")
-    printfn "%A" (Run a @"+.2")
-    printfn "%A" (Run a @"+1.2")
-    printfn "%A" (Run a @".2")
-    printfn "%A" (Run a @"12.34")
-    printfn "%A" (Run a @"12.34e12")
-    printfn "%A" (Run a @"12.34e+12")
-    printfn "%A" (Run a @"12.34e-12")
     0
 
--- ParserCombinators/Tests.fs
+++ ParserCombinators/Tests.fs
@@ -1,1 +1,59 @@
 
+module ParserCombinators.Tests
+open NUnit.Framework
+open ParserCombinators.Core
+
+[<TestFixture>]
+type ``Integer parsing``() =
+    [<Test>]
+    member x.``Empty string should fail``() =
+        Assert.AreEqual((Run IntegerParser ""), (Run (pzero |>> int) ""))
+        
+    [<Test>]
+    member x.``One digit integers``() =
+        Assert.AreEqual((Run IntegerParser "0"), Success(0, []))
+        Assert.AreEqual((Run IntegerParser "1"), Success(1, []))
+        
+    [<Test>]
+    member x.``Multiple digit integers``() =
+        Assert.AreEqual((Run IntegerParser "0123"), Success(123, []))
+        Assert.AreEqual((Run IntegerParser "123"), Success(123, []))
+        
+    [<Test>]
+    member x.``Integer with signs``() =
+        Assert.AreEqual((Run IntegerParser "+0123"), Success(123, []))
+        Assert.AreEqual((Run IntegerParser "+123"), Success(123, []))
+        Assert.AreEqual((Run IntegerParser "-0123"), Success(-123, []))
+        Assert.AreEqual((Run IntegerParser "-123"), Success(-123, []))
+    
+    
+[<TestFixture>]
+type ``Float parsing``() =
+    [<Test>]
+    member x.``Parsing zero``() =
+        Assert.AreEqual((Run FloatParser "0"), Success(0.0, []))
+        Assert.AreEqual((Run FloatParser ".0"), Success(0.0, []))
+        Assert.AreEqual((Run FloatParser "-0"), Success(0.0, []))
+        Assert.AreEqual((Run FloatParser "+0"), Success(0.0, []))
+        Assert.AreEqual((Run FloatParser "-0.0"), Success(0.0, []))
+        
+    [<Test>]
+    member x.``Parsing integers``() =
+        Assert.AreEqual((Run FloatParser "123"), Success(123.0, []))
+        Assert.AreEqual((Run FloatParser "+123"), Success(123.0, []))
+        Assert.AreEqual((Run FloatParser "-123"), Success(-123.0, []))
+        
+    [<Test>]
+    member x.``Floats without leading digits``() =
+        Assert.AreEqual((Run FloatParser ".012"), Success(0.012, []))
+        Assert.AreEqual((Run FloatParser ".0"), Success(0.0, []))
+        Assert.AreEqual((Run FloatParser "+.012"), Success(0.012, []))
+        Assert.AreEqual((Run FloatParser "-.012"), Success(-0.012, []))
+        
+    [<Test>]
+    member x.``Floats in scientific notation``() =
+        Assert.AreEqual((Run FloatParser "1.23e0"), Success(1.23e0, []))
+        Assert.AreEqual((Run FloatParser ".0e12"), Success(0.0, []))
+        Assert.AreEqual((Run FloatParser "+.012e-12"), Success(0.012e-12, []))
+        Assert.AreEqual((Run FloatParser "-.012E+1"), Success(-0.012e1, []))
+        
