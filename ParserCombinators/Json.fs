e7239d2e938f40a66025559cf536588d1475ddb7
santi <santialbo@gmail.com>
2013-03-20 14:15:26 -0400

    Added Json module for parsing json files

--- ParserCombinators/Json.fs
+++ ParserCombinators/Json.fs
@@ -1,1 +1,32 @@
 
+module ParserCombinators.Json
+
+open ParserCombinators.Core
+
+let AnsiLetterParser = CharParserF (fun c -> let c' = System.Char.ToLower c in c' >= 'a' && c <= 'z')
+
+let DigitCharParser = CharParserF (fun c -> c >= '0' && c <= '9')
+
+let KeyParser =
+    parse {
+        let! x = AnsiLetterParser <|> CharParser '_' 
+        let! xs = Many (AnsiLetterParser <|> DigitCharParser <|> CharParser '_')
+        return new System.String(x::xs |> List.toArray)
+    }
+    |> Between (CharParser '"') (CharParser '"') 
+    
+let ScapedString =
+    let EscapedChars = [('t', '\t'); ('n', '\n'); ('r', '\r'); ('"', '"')] |> Map.ofList
+    let NotEscapedCharParser = CharParserF (fun c -> not (c = '\\'))
+    let EscapedCharsParser =
+        parse {
+            let! i = CharParser '\\'
+            let! c = CharParserF EscapedChars.ContainsKey
+            return EscapedChars.[c]
+        }
+    in parse {
+        let! xs = Many (NotEscapedCharParser <|> EscapedCharsParser)
+        return new System.String(xs |> List.toArray)
+    }
+
+let N
--- ParserCombinators/Program.fs
+++ ParserCombinators/Program.fs
@@ -2,22 +2,14 @@
 module ParserCombinators.Main
 
 open ParserCombinators.Core
+open ParserCombinators.Json
 
-let letter = CharParserF (fun c ->
-         let c' = System.Char.ToLower c;
-         c' >= 'a' && c <= 'z')
-let digit = CharParserF (fun c -> c >= '0' && c <= '9')
-let symbol = CharParser '_' <|> CharParser '$'
-let keyName = (letter <|> symbol) .>>. many (letter <|> digit <|> symbol) |>> (fun (x, xs) -> new System.String(x::xs |> List.toArray))
 
-let key = keyName |> Between (CharParser '\"') (CharParser '\"') 
          
 
 [<EntryPoint>]
 let main args =
-    let testp = key
-    printfn "%A" (Run testp @"""test_name123""")
-    printfn "%A" (Run testp @"""$test_name""")
-    printfn "%A" (Run testp @"""_test_name""")
+    let a = ScapedString
+    printfn "%A" (Run a @"\n\t\""hola")
     0
 
